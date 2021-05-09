51204b485b3054bbaa5b6bfc414c32e114d4695d
panchami6 <62180655+panchami6@users.noreply.github.com>
2021-05-09 16:22:04 +0530

    deleted extra files

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
-import {VideoProvider} from "../src/Context/video-context";
 import { PlaylistProvider } from './Context/playlist-context';
 import { CustomPlaylistProvider } from './Context/customPlaylist-context';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <VideoProvider>
         <PlaylistProvider>
           <CustomPlaylistProvider>
             <App />
         </CustomPlaylistProvider>
         </PlaylistProvider>
-      </VideoProvider>  
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
